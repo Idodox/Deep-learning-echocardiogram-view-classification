5cf1d2ff072c8c6025def294da2e9c9523038d2a
Ido Farhi <idofarhi@Idos-MacBook-Pro.local>
2020-03-01 15:27:27 +0200

    organized a bit and starting visualizations

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # us-view-classification
 DL based echocardiogram view classification
+
+Masters thesis research in cooperation with prof. Dan Adam, Technion institute of technology, Israel.
--- helper_methods.py
+++ helper_methods.py
@@ -221,19 +221,19 @@ def calc_accuracy(prediction_list):
     return np.round(num_correct/video_count*100, 23)
 
 
-import sys
-import psutil
-def memReport():
-    for obj in gc.get_objects():
-        if torch.is_tensor(obj):
-            print(type(obj), obj.size())
-
-def cpuStats():
-        print(sys.version)
-        print(psutil.cpu_percent())
-        print(psutil.virtual_memory())  # physical memory usage
-        pid = os.getpid()
-        py = psutil.Process(pid)
-        memoryUse = py.memory_info()[0] / 2. ** 30  # memory use in GB...I think
-        print('memory GB:', memoryUse)
-
+# import sys
+# import psutil
+# def memReport():
+#     for obj in gc.get_objects():
+#         if torch.is_tensor(obj):
+#             print(type(obj), obj.size())
+#
+# def cpuStats():
+#         print(sys.version)
+#         print(psutil.cpu_percent())
+#         print(psutil.virtual_memory())  # physical memory usage
+#         pid = os.getpid()
+#         py = psutil.Process(pid)
+#         memoryUse = py.memory_info()[0] / 2. ** 30  # memory use in GB...I think
+#         print('memory GB:', memoryUse)
+#
