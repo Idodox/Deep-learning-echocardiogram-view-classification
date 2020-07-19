7f788d1d7fb9e58339f5818ed8e55f6a07fee6b7
Ido Farhi <idofarhi@Idos-MacBook-Pro.local>
2020-07-19 12:35:34 +0300

    added support for test set data creation

--- dataset_creation/create_n_step_sequences.py
+++ dataset_creation/create_n_step_sequences.py
@@ -6,15 +6,21 @@ from tqdm import tqdm
 
 from utils import get_frame_sequences, mkdir_if_missing, get_files_list, load_and_process_images
 
+"""
+This file takes in frames and creates pickle files that contains "min_frames" number of frames spaced "step_size" from each other.
+It also reshapes the frames to the "final_dim" size.
+"""
+
 ############### CONFIG ########################
 
 # view_list = ['apex', 'mitral', 'papillary']
 view_list = ['2CH', '3CH', '4CH', 'apex', 'mitral', 'papillary']
 step_size = 5
 min_frames = 10
-final_dim = 250
+final_dim = 100
 
-# source_directory = {}
+# Name for resulting dataset folder
+folder_name = str(min_frames)+'frame_'+str(step_size)+'steps_'+str(final_dim)+'px'
 
 source_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/apex',
                     'mitral': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/mitral',
@@ -22,8 +28,6 @@ source_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/ap
                     '2CH': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/2CH',
                     '3CH': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/3CH',
                     '4CH': '/Users/idofarhi/Documents/Thesis/Data/frames/raw/4CH'}
-
-folder_name = str(min_frames)+'frame_'+str(step_size)+'steps_'+str(final_dim)+'px'
 
 target_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/frames/' + folder_name + '/apex',
                     'mitral': '/Users/idofarhi/Documents/Thesis/Data/frames/' + folder_name + '/mitral',
@@ -33,13 +37,26 @@ target_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/frames/' + fo
                     '4CH': '/Users/idofarhi/Documents/Thesis/Data/frames/' + folder_name + '/4CH'
                     }
 
+# Test data:
+# source_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/raw/apex',
+#                     'mitral': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/raw/mitral',
+#                     'papillary': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/raw/papillary'}
+#
+# target_directory = {'apex': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/' + folder_name + '/apex',
+#                     'mitral': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/' + folder_name + '/mitral',
+#                     'papillary': '/Users/idofarhi/Documents/Thesis/Data/test_set/frames/' + folder_name + '/papillary'}
+
+
+
+
+
 #################################################
 
 for folder in target_directory.values():
     mkdir_if_missing(folder)
 
 video_list = {}
-file_names = get_frame_sequences('/Users/idofarhi/Documents/Thesis/Data/frames/raw', class_folders=['apex', 'papillary', 'mitral', '2CH', '3CH', '4CH'])
+file_names = get_frame_sequences(source_directory['apex'][:-5], class_folders= view_list)
 for view in view_list:
     video_list[view] = list(file_names[view].keys())
 
@@ -64,6 +81,7 @@ for view in view_list:
         # get video number of frames and make sure we can make
         # the needed number of sequences with at least min_frames each, else skip
         if len(video_frame_list) < min_frames * step_size:
+            print("File {} doesn't have minimum number of frames required, it has {} frames. Skipping.".format(video, len(video_frame_list)))
             continue
 
         for i in range(step_size):  # if this is 5: to get frames 0,5,10 etc then 1, 6, 11 etc ...
